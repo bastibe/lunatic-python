58f9e68d797460d98ce2ade9309d9cc4f002dac9
Greg Ward <greg@gerg.ca>
2013-02-19 13:12:07 -0500

    setup: reformat to make pep8 happy

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
+
+import sys
+import os
+import commands
+
 from distutils.core import setup, Extension
 from distutils.sysconfig import get_python_lib, get_python_version
-import sys
-import commands
-import os
 
 if os.path.isfile("MANIFEST"):
     os.unlink("MANIFEST")
 
 # You may have to change these
-PYLIBS = ["python"+get_python_version(), "pthread", "util"]
-PYLIBDIR = [get_python_lib(standard_lib=True)+"/config"]
+PYLIBS = ["python" + get_python_version(), "pthread", "util"]
+PYLIBDIR = [get_python_lib(standard_lib=True) + "/config"]
 LUALIBS = ["lua5.1"]
 LUALIBDIR = []
+
 
 def pkgconfig(*packages, **kwargs):
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
@@ -23,38 +26,37 @@ def pkgconfig(*packages, **kwargs):
         sys.exit("pkg-config failed: " + pcoutput)
 
     for token in pcoutput.split():
-        if flag_map.has_key(token[:2]):
+        if token[:2] in flag_map:
             kwargs.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-        else: # throw others to extra_link_args
+        else:                           # throw others to extra_link_args
             kwargs.setdefault('extra_link_args', []).append(token)
 
-    for k, v in kwargs.iteritems(): # remove duplicated
+    for k, v in kwargs.iteritems():     # remove duplicated
         kwargs[k] = list(set(v))
 
     return kwargs
 
 setup(name="lunatic-python",
-      version = "1.0",
-      description = "Two-way bridge between Python and Lua",
-      author = "Gustavo Niemeyer",
-      author_email = "gustavo@niemeyer.net",
-      url = "http://labix.org/lunatic-python",
-      license = "LGPL",
-      long_description =
-"""\
+      version="1.0",
+      description="Two-way bridge between Python and Lua",
+      author="Gustavo Niemeyer",
+      author_email="gustavo@niemeyer.net",
+      url="http://labix.org/lunatic-python",
+      license="LGPL",
+      long_description="""\
 Lunatic Python is a two-way bridge between Python and Lua, allowing these
 languages to intercommunicate. Being two-way means that it allows Lua inside
 Python, Python inside Lua, Lua inside Python inside Lua, Python inside Lua
 inside Python, and so on.
 """,
-      ext_modules = [
-                     Extension("lua-python",
-                               ["src/pythoninlua.c", "src/luainpython.c"],
-                               **pkgconfig('lua')
-                     ),
-                     Extension("lua",
-                               ["src/pythoninlua.c", "src/luainpython.c"],
-                               **pkgconfig('lua')
-                     )
-                    ],
+      ext_modules=[
+        Extension("lua-python",
+                  ["src/pythoninlua.c", "src/luainpython.c"],
+                  **pkgconfig('lua')
+                  ),
+        Extension("lua",
+                  ["src/pythoninlua.c", "src/luainpython.c"],
+                  **pkgconfig('lua')
+                  )
+        ],
       )
