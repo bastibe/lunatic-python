990a0f884ab23b209c3312a55324b9d9fa481ecc
Dmitry Stepanenko <dmitry.stepanenko@revelsystems.com>
2015-08-27 13:29:40 +0200

    specify extra_compile_args

--- setup.py
+++ setup.py
@@ -56,7 +56,8 @@ def pkgconfig(*packages):
 
     return kwargs
 
-lua_pkgconfig = pkgconfig('lua', 'lua' + LUAVERSION,'python-' + PYTHONVERSION)
+lua_pkgconfig = pkgconfig('lua' + LUAVERSION, 'lua' + LUAVERSION,'python-' + PYTHONVERSION)
+lua_pkgconfig['extra_compile_args'] = ['-I/usr/include/lua'+LUAVERSION]
 
 setup(name="lunatic-python",
       version="1.0",
