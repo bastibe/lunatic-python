5fb9b4933a1ab9502991ca66c14214b834c9395d
Greatwolf <github.greatwolf@mamber.net>
2017-03-02 03:39:10 -0800

    Added string test.

--- tests/test_py.lua
+++ tests/test_py.lua
@@ -7,9 +7,13 @@ assert(false  == python.eval "False")
 assert(1   == python.eval "1")
 assert(1.5 == python.eval "1.5")
 
+assert("foo" == python.eval "b'foo'")
+assert("bar" == python.eval "u'bar'")
+
 pyglob = python.globals()
 d = {}
 pyglob.d = d
 -- This crashes if you've compiled the python.so to include another
 -- Lua interpreter, i.e., with -llua.
 python.execute "d['key'] = 'value'"
+assert(d.key == 'value')
