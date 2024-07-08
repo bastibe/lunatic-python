406b46e3c6d55c34a2bba3b29b80c50187928576
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2024-07-08 06:05:49 +0000

    Edited code to remove unnecessary commented lines

--- src/luainpython.c
+++ src/luainpython.c
@@ -364,7 +364,6 @@ static PyObject* LuaObject_richcmp(PyObject *lhs, PyObject *rhs, int op)
     PyErr_SetString(PyExc_RuntimeError, lua_tostring(LuaState, -1));
     return NULL;
   }
-  //return lua_toboolean(LuaState, -1) ? Py_True : Py_False;
   return LuaConvert(LuaState, -1);
 }
 
@@ -461,62 +460,6 @@ PyTypeObject LuaObject_Type = {
     .tp_new = PyType_GenericNew,
     .tp_free = PyObject_Del,
 };
-
-/*
-PyTypeObject LuaObject_Type = {
-    PyVarObject_HEAD_INIT(NULL, 0)
-    "lua.custom",             //tp_name
-    sizeof(LuaObject),        //tp_basicsize
-    0,                        //tp_itemsize
-    (destructor)LuaObject_dealloc, //tp_dealloc
-    0,                        //tp_vectorcall_offset
-    0,                        //tp_getattr
-    0,                        //tp_setattr
-    0,                        //tp_compare
-    LuaObject_str,            //tp_repr
-    0,                        //tp_as_number
-    0,                        //tp_as_sequence
-    &LuaObject_as_mapping,    //tp_as_mapping
-    0,                        //tp_hash
-    (ternaryfunc)LuaObject_call,     //tp_call
-    LuaObject_str,            //tp_str
-    LuaObject_getattr,       //tp_getattro
-    LuaObject_setattr,        //tp_setattro
-    0,                        //tp_as_buffer
-    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, //tp_flags
-    "custom lua object",      //tp_doc
-    0,                        //tp_traverse
-    0,                        //tp_clear
-    LuaObject_richcmp,        //tp_richcompare
-    0,                        //tp_weaklistoffset
-    PyObject_SelfIter,        //tp_iter
-    (iternextfunc)LuaObject_iternext, //tp_iternext
-    0,                        //tp_methods
-    0,                        //tp_members
-    0,                        //tp_getset
-    0,                        //tp_base
-    0,                        //tp_dict
-    0,                        //tp_descr_get
-    0,                        //tp_descr_set
-    0,                        //tp_dictoffset
-    0,                        //tp_init
-    PyType_GenericAlloc,      //tp_alloc
-    PyType_GenericNew,        //tp_new
-    PyObject_Del,            //tp_free
-    0,                        //tp_is_gc
-    0,  // tp_bases
-    0, // tp_mro
-    0, // tp_cache
-    0, // tp_subclasses
-    0, //tp_weaklist   
-    0, //tp_del
-    0, //tp_version_tag
-    0, //tp_finalize
-    0, //tp_vectorcall
-    0, //tp_watched
-};
-*/
-
 
 PyObject *Lua_run(PyObject *args, int eval)
 {
